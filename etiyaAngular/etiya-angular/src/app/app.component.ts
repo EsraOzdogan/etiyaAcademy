5d8b76500cbe391ee35191f2294c6a2fc1a21ab8
EsraOzdogan <esraozdogaan@gmail.com>
2022-07-25 19:06:16 +0300

    etiya-angular initial commit

---
+++ etiyaAngular/etiya-angular/src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',   //componentin nasıl cagıralacagı
+  templateUrl: './app.component.html',  //template:`<h2>Etiya</h2>` -->böyle html de yazılır
+  styleUrls: ['./app.component.css']  //Array--birden fazla style dosyaları alınabilir
+})
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    // throw new Error('Method not implemented.');
+    console.log('app.component init fonksiyonu')
+  }
+  title = 'etiya-angular';
+  name = "Esra Özdoğan";
+
+  counter:number = 0;
+
+  // increase(){
+  //   this.counter ++;  //this.counter AppComponent sınıfındaki counter
+  // }
+
+
+  // increase(count:number){
+  //   this.counter += count;
+  // }
+
+  increase(count:number=1){ //dafeult
+    this.counter += count;
+  }
+
+  getCounter(){
+    return this.counter;
+  }
+
+  // get Counter(){   //getter
+  //   return this.counter;
+  // }
+
+  get Counter(){   //getter
+    return (
+      this.counter.toString() + (this.counter <5 ? 'Çok az' : 'Çok fazla')
+      )
+  }
+}
---
+++ etiyaAngular/etiya-angular/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { AddProductComponent } from './add-product/add-product.component';
+
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    AddProductComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
