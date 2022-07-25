c80909b4472776ea4f5d22fb1409419920bb99d1
EsraOzdogan <esraozdogaan@gmail.com>
2022-07-25 23:18:48 +0300

    oop

--- etiyaHomeworks/Hw1/src/oop/oop.js
+++ etiyaHomeworks/Hw1/src/oop/oop.js
@@ -1,19 +1,28 @@
-class Customer{
+class Customer{   //BaseCustomer   //export--public
     constructor(id,customerNumber){
         this.id=id
         this.customerNumber=customerNumber
     }
 }
-//prototyping
-let customer=new Customer(1,"1234")
-customer.name="Esra Özdoğan";
+
+
+let customer=new Customer(1,"12345")   //Nesne olusturma(instance) //Customer customer = new Custoemr() yok
+
+console.log(customer.id) //constructor icinde this.id= id ile id propertysi olusturup paramtre id ile eslestirmezsen calismaz
+
+//prototyping--Bir nesneye yeni özellik ekleme
+customer.name="Esra Özdoğan";   //instance prototyping--Bir nesneye yeni özellik ekleme
 console.log(customer.name)
 console.log(customer.customerNumber)
+
+Customer.bisey = "Bişey";        //class prototyping
+console.log(Customer.bisey)
+
 
 
 class IndividualCustomer extends Customer{
     constructor(firstName,id,customerNumber){
-        super(id,customerNumber)
+        super(id,customerNumber)  //base'e gönderdik yani Customer sinifina
         this.firstName = firstName
     }
 }
