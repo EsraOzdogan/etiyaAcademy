2ec0d5bb5d1dfeedf4af00356c7227893bb72e29
EsraOzdogan <esraozdogaan@gmail.com>
2022-07-27 03:00:28 +0300

    kodlama.io clone

--- kodlamaio-clone/src/app/app-routing.module.ts
+++ kodlamaio-clone/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { SssComponent } from './pages/sss/sss.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:"", pathMatch:"full", component:HomePageComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'sss', component: SssComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- kodlamaio-clone/src/app/app.module.ts
+++ kodlamaio-clone/src/app/app.module.ts
@@ -4,11 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './common/nav/nav.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SssComponent } from './pages/sss/sss.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { FilterSearchBarComponent } from './components/filter-search-bar/filter-search-bar.component';
+import { LessonCardComponent } from './components/lesson-card/lesson-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomePageComponent,
+    SssComponent,
+    LoginComponent,
+    RegisterComponent,
+    FooterComponent,
+    FilterSearchBarComponent,
+    LessonCardComponent
   ],
   imports: [
     BrowserModule,
