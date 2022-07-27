ac7e1e85d21a0e844ec42760d532f58310dd1c76
EsraOzdogan <esraozdogaan@gmail.com>
2022-07-27 13:54:37 +0300

    refactor add-product

---
+++ etiyaAngular/etiya-angular/src/app/pages/add-product/add-product.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-product',
+  templateUrl: './add-product.component.html',
+  styleUrls: ['./add-product.component.css']
+})
+export class AddProductComponent implements OnInit {
+  email!:string;  //baslangic degeri yok  //inputtaki degerin anında degismesi ve input degerinin de anında alınması icin ngModule
+  password!:string;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.email = "e@gmail.com";
+    //this.onEmailChange(); //this.email ="e@gmail.com" olarak yazıldığında çalışmıyor o nedenle
+  }
+
+  // onEmailChange(){
+  //   console.log(`Değer değişti: ${this.email}`)
+  // }
+
+  // onEmailChange(event:string){
+  //   console.log(`Değer değişti: ${event}`)
+  // }
+
+  onEmailChange(event:string){
+    if(!event.includes("@")){
+      console.log("Hatalı çalıştı")
+    }
+    else{
+      console.log(`Değer değişti: ${event}`)
+    }
+  }
+
+
+  changeEmail(){
+    this.email = "blabla";
+  }
+}
