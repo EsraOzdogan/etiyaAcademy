0578ed31fe559049781749ca8907343bdc643f6e
EsraOzdogan <esraozdogaan@gmail.com>
2022-07-27 16:17:12 +0300

    get data with input directive

--- kodlamaio-clone/src/app/components/lesson-card/lesson-card.component.ts
+++ kodlamaio-clone/src/app/components/lesson-card/lesson-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-lesson-card',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lesson-card.component.css']
 })
 export class LessonCardComponent implements OnInit {
-
+  @Input() lesson: any;
   constructor() { }
 
   ngOnInit(): void {
--- kodlamaio-clone/src/app/pages/home-page/home-page.component.ts
+++ kodlamaio-clone/src/app/pages/home-page/home-page.component.ts
@@ -6,10 +6,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
+  lessonList !: any[]
 
   constructor() { }
 
   ngOnInit(): void {
+    this.getLessonApi()
   }
 
+  getLessonApi(){
+    this.lessonList=[
+      {
+      "id": 1,
+      "lessonName": "Yazılım Geliştirici Yetiştirme Kampı (JavaScript)",
+      "lessonImg": "assets/img/lesson1.jpg",
+      "description": "1,5 ay sürecek Yazılım Geliştirici Yetiştirme Kampımızın takip, döküman ve duyurularını buradan yapacağız.",
+      "instructorName": "Engin Demiroğ",
+      "instructorImg": "assets/img/engin-demirog.jpg",
+      "price": "Ücretsiz"
+     },
+     {
+      "id": 2,
+      "lessonName": "Yazılım Geliştirici Yetiştirme Kampı (C# + ANGULAR)",
+      "lessonImg": "assets/img/lesson2.jpg",
+      "description": "2 ay sürecek Yazılım Geliştirici Yetiştirme Kampımızın takip, döküman ve duyurularını buradan yapacağız.",
+      "instructorName": "Engin Demiroğ",
+      "instructorImg": "assets/img/engin-demirog.jpg",
+      "price": "Ücretsiz"
+     },
+     {
+      "id": 3,
+      "lessonName": "Yazılım Geliştirici Yetiştirme Kampı (JAVA + REACT)",
+      "lessonImg": "assets/img/lesson3.jpg",
+      "description": "2 ay sürecek Yazılım Geliştirici Yetiştirme Kampımızın takip, döküman ve duyurularını buradan yapacağız.",
+      "instructorName": "Engin Demiroğ",
+      "instructorImg": "assets/img/engin-demirog.jpg",
+      "price": "Ücretsiz"
+     },
+     {
+      "id": 4,
+      "lessonName": "Programlamaya Giriş İçin Temel Kurs",
+      "lessonImg": "assets/img/lesson4.jpg",
+      "description": "PYTHON, JAVA, C# gibi tüm programlama dilleri için temel programlama mantığını anlaşılır örneklerle öğrenin.",
+      "instructorName": "Engin Demiroğ",
+      "instructorImg": "assets/img/engin-demirog.jpg",
+      "price": "Ücretsiz"
+     }
+  ]
+}
 }
