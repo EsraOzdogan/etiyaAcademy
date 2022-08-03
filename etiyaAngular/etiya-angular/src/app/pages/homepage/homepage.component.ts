224e58b2495f415c9e28751e58c9c1afc8d2f7b2
EsraOzdogan <esraozdogaan@gmail.com>
2022-08-03 14:51:43 +0300

    added date pipe

--- etiyaAngular/etiya-angular/src/app/pages/homepage/homepage.component.ts
+++ etiyaAngular/etiya-angular/src/app/pages/homepage/homepage.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
-
+  today: Date = new Date();
   constructor() { }
 
   ngOnInit(): void {
