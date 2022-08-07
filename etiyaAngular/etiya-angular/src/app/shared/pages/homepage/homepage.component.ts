0a7f78d7350b02be14d3c463b62ffead834ce6a6
EsraOzdogan <esraozdogaan@gmail.com>
2022-08-08 00:59:46 +0300

    refactored the project with modules

---
+++ etiyaAngular/etiya-angular/src/app/shared/pages/homepage/homepage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.css']
+})
+export class HomepageComponent implements OnInit {
+  today: Date = new Date();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
