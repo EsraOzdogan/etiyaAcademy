fc42459deac46f3facdeb631a9f20e50ec80eb1e
EsraOzdogan <esraozdogaan@gmail.com>
2022-08-10 01:40:36 +0300

    Added navbar and footer

--- etiyaAngular/etiya-telco/etiya-telcom/src/app/app.module.ts
+++ etiyaAngular/etiya-telco/etiya-telcom/src/app/app.module.ts
@@ -4,13 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {ButtonModule} from 'primeng/button';
+import { SharedModule } from './shared/shared.module';
+import { FeaturesModule } from './features/features.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    SharedModule,
+    FeaturesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ etiyaAngular/etiya-telco/etiya-telcom/src/app/features/features.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FeaturesRoutingModule } from './features-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FeaturesRoutingModule
+  ]
+})
+export class FeaturesModule { }
