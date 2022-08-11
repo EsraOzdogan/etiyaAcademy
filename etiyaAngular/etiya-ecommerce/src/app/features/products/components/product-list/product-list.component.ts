886397016d750d44f7af315c7acc1d6aeef2115f
EsraOzdogan <esraozdogaan@gmail.com>
2022-08-11 16:32:05 +0300

    fixed

--- etiyaAngular/etiya-ecommerce/src/app/features/products/components/product-list/product-list.component.ts
+++ etiyaAngular/etiya-ecommerce/src/app/features/products/components/product-list/product-list.component.ts
@@ -1,5 +1,5 @@
 import { Product } from './../../models/product';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import { ProductsService } from '../../services/products/products.service';
 
 @Component({
@@ -7,20 +7,58 @@ import { ProductsService } from '../../services/products/products.service';
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnChanges {
   productList! : Product[];
   @Input() categoryId! : number;
+  fetchStatus = "pending";
+  pageSize = 10; //sayfa basina düsen ürün ngFor
+  //pageSizeList = []
 
   constructor(private productsService: ProductsService) { }
 
-  ngOnInit(): void {
-    this.getAllProducts();
+  ngOnChanges(changes: SimpleChanges): void {     //Componentteki degisikliklere takip etmek icin
+    console.log(changes);
+    this.getAllProductsByCategoryId();
   }
 
-  getAllProducts(){
-    this.productsService.getAll().subscribe((response => {
-      this.productList = response;
-    }))
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.getAllProducts();
+    }, 1500);
+
+    //this.pageSizeList.length = this.pageSize
+
+  }
+
+  getAllProductsByCategoryId() {
+    if (!this.categoryId || this.categoryId <= 0) {        //Category'de All durumunda categoryId productsServicedeki getAllByCategories fonksiyonuna undefined gidecek bu nedenle kontrol ediyoruz. Categorylerin listelendiği itemslarda queryParamsda categoryId'ye backend ile anlasıp 0 vererek  kontrol edebiliriz.Ama gerek yok suan
+      this.getAllProducts();
+      return;
+    }
+    this.fetchStatus = 'pending';
+    setTimeout(() => {
+      this.productsService.getAllByCategories(this.categoryId).subscribe(
+        (response) => {
+          this.productList = response;
+          this.fetchStatus = 'loaded';
+        },
+        (error) => {
+          this.fetchStatus = 'error';
+        }
+      );
+    }, 2000);
+  }
+
+  getAllProducts() {
+    this.productsService.getAll().subscribe(
+      (response) => {
+        this.productList = response;
+        this.fetchStatus = 'loaded';
+      },
+      (error) => {
+        this.fetchStatus = 'error';
+      }
+    );
   }
 
 }
