419d6cffd981c9030719fa2a47fb97cc1d53512f
EsraOzdogan <esraozdogaan@gmail.com>
2022-08-11 09:55:42 +0300

    fixed product-list

--- etiyaAngular/etiya-ecommerce/src/app/features/products/components/product-list/product-list.component.ts
+++ etiyaAngular/etiya-ecommerce/src/app/features/products/components/product-list/product-list.component.ts
@@ -3,21 +3,21 @@ import { Component, OnInit } from '@angular/core';
 import { ProductsService } from '../../services/products/products.service';
 
 @Component({
-  selector: 'app-product-list',
+  selector: 'etiya-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-  productList !: Product[];
+  productList! : Product[];
 
-  constructor(private productService: ProductsService) { }
+  constructor(private productsService: ProductsService) { }
 
   ngOnInit(): void {
     this.getAllProducts();
   }
 
   getAllProducts(){
-    this.productService.getAll().subscribe((response => {
+    this.productsService.getAll().subscribe((response => {
       this.productList = response;
     }))
   }
