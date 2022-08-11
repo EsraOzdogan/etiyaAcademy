2bae04e078eeb7568ee216783aeafe2b5b550415
EsraOzdogan <esraozdogaan@gmail.com>
2022-08-11 10:55:11 +0300

    added dashboard-layout

--- etiyaAngular/etiya-telco/etiya-telco/src/app/shared/shared.module.ts
+++ etiyaAngular/etiya-telco/etiya-telco/src/app/shared/shared.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { DashboardLayoutComponent } from './layouts/dashboard-layout/dashboard-layout.component';
 
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    DashboardLayoutComponent
   ],
   imports: [
     CommonModule
   ],
   exports : [
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    DashboardLayoutComponent
   ]
 })
 export class SharedModule { }
