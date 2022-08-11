85b1be1e354cacbc787f9b9797f1fd082544c639
EsraOzdogan <esraozdogaan@gmail.com>
2022-08-11 13:10:50 +0300

    added service-dashboard

---
+++ etiyaAngular/etiya-telco/etiya-telco/src/app/features/services/pages/service-dashboard/service-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-service-dashboard',
+  templateUrl: './service-dashboard.component.html',
+  styleUrls: ['./service-dashboard.component.css']
+})
+export class ServiceDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- etiyaAngular/etiya-telco/etiya-telco/src/app/features/services/services.module.ts
+++ etiyaAngular/etiya-telco/etiya-telco/src/app/features/services/services.module.ts
@@ -1,4 +1,3 @@
-import { SharedModule } from './../../shared/shared.module';
 import { ServiceFormComponent } from './components/service-form/service-form.component';
 import { ServiceListComponent } from './components/service-list/service-list.component';
 import { NgModule } from '@angular/core';
@@ -10,11 +9,13 @@ import {TableModule} from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
 import {ToolbarModule} from 'primeng/toolbar';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ServiceDashboardComponent } from './pages/service-dashboard/service-dashboard.component';
 
 @NgModule({
   declarations: [
     ServiceListComponent,
-    ServiceFormComponent
+    ServiceFormComponent,
+    ServiceDashboardComponent
   ],
   imports: [
     CommonModule,
@@ -24,7 +25,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     ToolbarModule,
     FormsModule,
     ReactiveFormsModule,
-    SharedModule
   ],
   exports : [
     ServiceListComponent
