efc904ad1b0e1d80108770f793a85b37c1a0a1f6
EsraOzdogan <esraozdogaan@gmail.com>
2022-08-15 00:29:07 +0300

    fixed

--- etiyaAngular/etiya-telco/etiya-telco/src/app/features/services/services-routing.module.ts
+++ etiyaAngular/etiya-telco/etiya-telco/src/app/features/services/services-routing.module.ts
@@ -1,10 +1,11 @@
+import { HomepageComponent } from './../common/pages/homepage/homepage.component';
 import { ServiceFormComponent } from './components/service-form/service-form.component';
 import { ServiceListComponent } from './components/service-list/service-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path:'services',component:ServiceListComponent},
+  {path:'services',component:HomepageComponent},
   {path:'add',component:ServiceFormComponent},
   {path:'service/:id',component:ServiceFormComponent},
 ];
