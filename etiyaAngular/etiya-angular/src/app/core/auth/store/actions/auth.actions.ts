52b404092566491201c8dad5e30eabbd50eb99fb
Esra Özdoğan <50195220+EsraOzdogan@users.noreply.github.com>
2022-08-30 19:44:03 +0300

    refactor

--- etiyaAngular/etiya-angular/src/app/core/auth/store/actions/auth.actions.ts
+++ etiyaAngular/etiya-angular/src/app/core/auth/store/actions/auth.actions.ts
@@ -2,6 +2,9 @@ import { createAction, props } from '@ngrx/store';
 
 import { TokenUserModel } from '../../models/tokenUserModel';
 
+// createAction => fonksiyon/aksiyon oluştur
+// key => benzersiz '[Auth] Set Token User Model
+// props => fonksiyonun içindeki propslar, parametreler
 export const setTokenUserModel = createAction(
   '[Auth] Set Token User Model', // benzersiz key
   props<{ tokenUserModel: TokenUserModel }>() // parametreler
--- etiyaAngular/etiya-angular/src/app/core/auth/store/reducers/auth.reducer.ts
+++ etiyaAngular/etiya-angular/src/app/core/auth/store/reducers/auth.reducer.ts
@@ -26,12 +26,21 @@ const initialAuthState: AuthState = {
 //   tokenUserModel: undefined
 // }; // x05312132
 
+
+// 1. parametre initial state => ilk durum girisYapti=false, token = null
+// 2+ parametreler => oluşturduğumuz aksiyonların içini doldurur
 export const authReducer = createReducer(
   initialAuthState,
   on(setTokenUserModel, (state, action) => {        //setTokenUserModel aksiyonu
     // 1. paramtere o anki state değeri, 2. parametre üzerinde çalıştığımız action
+    // state => depodaki o an genel durum
+    // action => bu fonksiyon ve içeriği
+    // spread operatötü kullanma sebebimiz => depo bizden tüm state'i geri istiyor
+    // bizi ilgilendirmeyen (fonksiyonu kullanırken(setTokenUserModel)) bozmak istemiyoruz
+    // diğer tüm state'i olduğu gibi geri yolluyorum
     return { ...state, tokenUserModel: action.tokenUserModel };  //aynı sey ama bircok state durumunda kolay olur ...state     //return { auth: state.auth, test: state.test, tokenUserModel: action.tokenUserModel };
   }),
+  // sadece state alınarak da kullanılır. Tek parametre aldığı için paranteze alınmamış. Actionda herhangi bir işlem yapmıycam. Zaten removeTokenUserModel fonksiyonunda prosu yok o nedenle action'a da gerek yok
   on(removeTokenUserModel, state => {        //removeTokenUserModel aksiyonu      //çıkış yaptıgımda tokenUserModel undefined olacak
     const newState: AuthState = { ...state, tokenUserModel: undefined }; // x48746546        //return { ...state, tokenUserModel: undefined }; durumunda tokenUserModel'e yeni bir atama olmuyor bu nedenle değisken tanımladık  //state.tokenUserModel = undefined diyip return sate demiyoruz çünkü bellek adresini tutuyor bellek adresi degismiycek. verinin kendisini tutuyor obje. Bu nednele state degiskenini degerinin degisip degismedigini anlamıyoruz . Bu nedenle bellek adresini degistirmek iin yeni obje olusturuyoruz authState : AuthState diye
     return newState;
